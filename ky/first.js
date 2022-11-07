bc769e205ba78fd4c8d5ece5280480ea92075af0
Rashid <mrrashangry@gmail.com>
2022-11-07 12:30:13 +0300

    сделано

---
+++ ky/first.js
@@ -0,0 +1,8 @@
+
+let summary = 14;
+let summary2 = 2;
+function sum(a, b) {
+    return a + b
+ }
+export default sum
+export {summary, summary2}
---
+++ ky/second.js
@@ -0,0 +1,4 @@
+import {summary, summary2} from './first.js'
+import sum from './first.js'
+console.log(sum(2, 4))
+console.log(summary)
